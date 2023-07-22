8df8b5435991657e4741b472cd4883441cf27d13
mel b <114832865+lovewitchwifi@users.noreply.github.com>
2023-07-22 17:47:57 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//Import route and our components
+import "./styles.css";
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/currencies";
+import Main from "./pages/main";
+import Price from "./pages/price";
+import Nav from "./components/nav";
+
+export default function App() {
+  // We will use the Route component to specify each route
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/currencies" element={<Currencies />} />
+        <Route path="/price/:symbol" element={<Price />} />
+      </Routes>
+    </div>
+  );
+}
